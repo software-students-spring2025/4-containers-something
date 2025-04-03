5f0b798cbca4dcde60b3f794323dce1b018a37df
ChrisimKim <ck3388@nyu.edu>
2025-04-02 22:59:45 -0400

    basic placeholder with needed files

--- web-app/app.py
+++ web-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,16 +7,15 @@ db = client["ml_database"]
 collection = db["sensor_data"]
 
 @app.route("/")
-def greet():
-    return "Hello, World!"
+def home():
+    return render_template("index.html")
 
 @app.route("/data")
 def get_data():
-    # Just show the last inserted document (for placeholder purposes)
     latest = collection.find_one(sort=[("_id", -1)])
     if latest:
         latest["_id"] = str(latest["_id"])  # Convert ObjectId to string for JSON
     return jsonify(latest or {"message": "No data found."})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")  # host="0.0.0.0" makes it accessible from other containers
+    app.run(debug=True, host="0.0.0.0", port=5001)
