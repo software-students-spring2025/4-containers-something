ebfde7dd8bf90d4dc32d7e8f086e609009f1e191
radishsoups <theobsidianheart@gmail.com>
2025-04-04 21:33:39 -0400

    Updating tests

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -7,6 +7,12 @@ from app import app
 
 
 @pytest.fixture
-def client():
+def test_client():
+    """Fixture to create a test client for the Flask app."""
     with app.test_client() as client:
         yield client
+
+
+def test_homepage(test_client):
+    response = test_client.get('/')
+    assert response.status_code == 200
