38f8b5b5e85b6f592d17cce526ef812795083c79
radishsoups <theobsidianheart@gmail.com>
2025-04-04 22:02:15 -0400

    Updating tests

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -7,12 +7,17 @@ from app import app
 
 
 @pytest.fixture
-def client():
-    """Fixture to create a test client for the Flask app."""
+def client_fixture():
+    """
+    Create a test client for the Flask application.
+    """
     with app.test_client() as client:
         yield client
 
-def test_homepage(client):
-    """Test the homepage."""
-    response = client.get('/')
+
+def test_home_route(client_fixture):  # pylint: disable=redefined-outer-name
+    """
+    Test the home route to ensure it returns a status code of 200.
+    """
+    response = client_fixture.get("/")
     assert response.status_code == 200
