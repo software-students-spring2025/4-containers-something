c2cba37d627c83b564342afabfafa284f04606e8
radishsoups <theobsidianheart@gmail.com>
2025-04-09 22:22:16 -0400

    Adding login feature

--- web-app/app.py
+++ web-app/app.py
@@ -5,15 +5,19 @@ the latest data collected and analyzed by the ML client.
 """
 
 import os
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect, request, flash, url_for
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# Load env variables
+# Load environment variables from the .env file
 load_dotenv()
 
+# Set the secret key for the session
+app.secret_key = os.getenv("SECRET_KEY")  # Get secret key from the .env file
+
+# Load MongoDB credentials
 username = os.getenv("MONGO_INITDB_ROOT_USERNAME")
 password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
 
@@ -21,25 +25,30 @@ password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
 client = MongoClient(os.getenv("URI"))
 db = client["ml_database"]
 collection = db["sensor_data"]
+users = db["users"]
 
 
 @app.route("/")
 def home():
     """Render the home page (index.html)."""
-
-    # Test DB connection
-    # collection.insert_one({"name": "Test", "sentence": "HELLO WORLD WAHOO"})
-
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    """Render the home page (index.html)."""
+    """Render the login page (login.html)."""
+    # POST request
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-    # Test DB connection
-    # collection.insert_one({"name": "Test", "sentence": "HELLO WORLD WAHOO"})
+        if users.find_one({"username": username, "password": password}):
+            return redirect(url_for('home', username=username))
+        else:
+            flash("Invalid username or password.", "danger")
+            return render_template("login.html")
 
+    # GET request
     return render_template("login.html")
 
 
@@ -53,5 +62,5 @@ def get_data():
 
 
 if __name__ == "__main__":
-    # start Flask development server
+    # Start Flask development server
     app.run(debug=True, host="0.0.0.0", port=5002)
